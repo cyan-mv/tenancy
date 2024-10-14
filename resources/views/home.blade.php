abdf421002b9ccfc3d04214fa2bd91460f18d216
cyan-mv <cyan.mv@gmail.com>
2024-10-14 12:49:18 -0600

    Include maryui plugin create initial posts layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,8 @@
         @foreach ($posts as $post)
             <div class="card w-full bg-base-100 shadow-xl">
                 <figure>
-                    <img src="{{ $post->image_url }}" alt="{{ $post->title }}" class="w-full h-48 object-cover">
+                    <img src="{{ asset('storage/' . $post->image_url) }}" alt="img-broken" class="w-full h-48 object-cover">
+
                 </figure>
                 <div class="card-body">
                     <h2 class="card-title">
@@ -20,5 +21,6 @@
                 </div>
             </div>
         @endforeach
+{{--        <p>{{ $post->image_url }}</p>--}}
     </div>
 @endsection
